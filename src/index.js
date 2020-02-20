07959300774ea2b222f509dc8987e58ed49812c6
drliubov <druzyaka.liubov@gmail.com>
2020-02-20 20:59:14 +0300

    feat: add solved Reverse Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let strNum = '';
+   n = Math.abs(n); 
+    while (n/10>0){
+        strNum += n%10;
+        n=Math.floor(n/10);
+    }
+    return parseInt(strNum);
 }
